1745f4875999cc6e478e5d35c281881ab46908c8
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2020-04-01 03:09:21 -0700

    deleting node cache

--- node_modules/netlify-cms-color-field/src/index.js
+++
@@ -1,9 +0,0 @@
-import Control from './Control'
-import Preview from './Preview'
-
-if (typeof window !== 'undefined') {
-  window.Control = Control
-  window.Preview = Preview
-}
-
-export { Control, Preview }
