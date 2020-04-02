2e27ca2987d524a43406500ce74c304d11034046
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2020-04-01 19:14:37 -0700

    Untracked files issue resolved to fix .gitignore

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "DharmaWorks - %s",
-  "siteUrl": "https://www.dharmaworks.com",
-  "version": "0.7.13",
-  "catchLinks": true
-}
