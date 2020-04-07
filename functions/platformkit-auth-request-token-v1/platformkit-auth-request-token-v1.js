92a9d8c9b4e97af1d0c060b1cdcc622c74799f5e
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2020-04-07 03:54:15 -0700

    Update platformkit-auth-request-token-v1.js

--- functions/platformkit-auth-request-token-v1/platformkit-auth-request-token-v1.js
+++ functions/platformkit-auth-request-token-v1/platformkit-auth-request-token-v1.js
@@ -112,6 +112,7 @@ exports.handler = async (event, context) => {
 
 
     } catch (err) {
+        console.log(err);
         return { statusCode: 500, body: JSON.stringify({ status: 500, data: null, error: true, message: err.toString() }, null, 3) }
     }
 }
