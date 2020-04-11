cdf425a32d40352a19560af2f1242dd45a095152
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2020-04-10 17:04:54 -0700

    cleanup

--- functions/platformkit-payments-checkout-v1/platformkit-payments-checkout-v1.js
+++ functions/platformkit-payments-checkout-v1/platformkit-payments-checkout-v1.js
@@ -1,6 +1,6 @@
 // Set your secret key. Remember to switch to your live secret key in production!
 // See your keys here: https://dashboard.stripe.com/account/apikeys
-const stripe = require('stripe')('***REMOVED***');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 (async () => {
   const session = await stripe.checkout.sessions.create({
