29494996a9bea4d2e19fa69cab8e770904b101fe
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2020-04-16 16:03:46 -0700

    debugging lambda

--- functions/platformkit-content-read-v1/platformkit-content-read-v1.js
+++ functions/platformkit-content-read-v1/platformkit-content-read-v1.js
@@ -53,7 +53,13 @@ exports.handler = async (event, context) => {
     if (tokenResult == true) {
 
       var meta = { user: {} };
-      var stripeTools = require('../../lib/stripe/stripe');
+      if(process.env.ENVIRONMENT == 'development'){
+        var stripeTools = require('../../lib/stripe/stripe');
+      }
+      else {
+        var stripeTools = require('/lib/stripe/stripe');
+      }
+      
       var user = new stripeTools(decodedUser.sub);
       var user = await user.getDetails();
       meta.user = user;
