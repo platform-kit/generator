cbe68219ffbe3242f1f72d03819fd79db6fae5ab
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2021-02-28 19:11:24 -0800

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,7 +4,7 @@
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-var companySettings = require("./data/company.json");
+var companySettings = require("data/company.json");
 
 var output = process.env.OUTPUT_DIR || 'dist';
 
